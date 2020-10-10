3a7b4ec3e78b81a277e23a52793de36425b869f9
kurikinton105 <yamadakenta0101@gmail.com>
2020-10-10 15:11:52 +0900

    ルーティングを実装

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import React from 'react';
 import Top from './screens/Top';
 import Navbar from './components/Navbar';
 import Quiz from './screens/Quiz';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
-  return ( //
-    <div className="App">
+  return (
+    <BrowserRouter>
       <Navbar />
-      <Top />
-      <Quiz />
-      {/* <Top></Top> */}
-      //
-    </div>
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={Top} />
+          <Route path="/quiz" component={Quiz} />
+          <Route render={() => <h4>not found...</h4>} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
+
 }
 
 export default App;
