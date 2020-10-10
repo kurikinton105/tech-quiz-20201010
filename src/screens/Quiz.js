6ebe9f44338b9f0d977418761f95420755085eff
kurikinton105 <yamadakenta0101@gmail.com>
2020-10-10 16:21:36 +0900

    add quiz api

--- src/screens/Quiz.js
+++ src/screens/Quiz.js
@@ -17,27 +17,77 @@ const config = {
 console.log(process.env.REACT_APP_QUIZAPI_KEY)
 
 const Quiz = () => {
+    // 保存する情報を定義
+    const [quizzes, setquizzes] = useState(null);
+    const [isLoading, setLoading] = useState(true);
+    const [selectedAnswers, setSelectedAnswers] = useState([]);
+    const [isCorrect, setCorrect] = useState(null);
+    const [answered, setAnswered] = useState(false);
+    
     useEffect(() => {
         fetchQuiz();
         return;
     }, []);
-     
     const fetchQuiz = () => {
-        // 情報を取得(重要)
-        axios //axios.get() でGETができる！！！
-          .get(url, config)
-          .then((res) => {
-            console.log(res);
-          })
-          .catch((e) => {
-            alert("エラーだよ。コンソールを見てね");
-            console.error(e);
-          })
-          .finally(() => {
-            console.log("done!");
-          });
-    };
+        setAnswered(false);
+        setLoading(true);
+        setSelectedAnswers([]);
+        axios
+            .get(url, config)
+            .then((res) => {
+            // console.log(res)
+                if (res.data) {
+                    setquizzes(res.data[0]);
+                }
+            })
+            .catch((e) => {
+                alert("エラーだよ。コンソールを見てね");
+                console.error(e);
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+        };
     
+        const checkAnswer = () => {
+        // 処理しやすい形に変換
+            const realAnswer = quizList
+            .map((item, index) => ({
+                index: index,
+                isCorrect: item.isCorrect,
+            }))
+            .filter((item) => item.isCorrect)
+            .map((item) => item.index)
+            .sort();
+            setCorrect(
+                JSON.stringify(realAnswer) === JSON.stringify(selectedAnswers.sort())
+            );
+            setAnswered(true);
+        };
+    
+        const updateAnswer = (id, append) => {
+            if (append) {
+                setSelectedAnswers([...selectedAnswers, id]);
+            } else {
+                setSelectedAnswers([...selectedAnswers].filter((item) => item !== id));
+            }
+        };
+    
+      // JSONは真偽値も表せるはずなのにstringで渡してきやがるからparseする
+      const correctAnswers =
+        quizzes &&
+        Object.values(quizzes.correct_answers).map((item) => JSON.parse(item));
+    
+      // 処理しやすい形にしておく
+      const quizList =
+        quizzes &&
+        Object.values(quizzes.answers)
+          .filter((choice) => !!choice)
+          .map((question, index) => ({
+            choice: question,
+            isCorrect: correctAnswers[index],
+          }));
+    console.log(quizList)
   return (
     <div>
 		<h2>クイズだよ！</h2>
