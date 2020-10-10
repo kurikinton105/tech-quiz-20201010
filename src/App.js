5be68668bef084d5073241f5f34c3ef6ac39b023
kurikinton105 <yamadakenta0101@gmail.com>
2020-10-10 15:05:59 +0900

    componentsの追加

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Top from './screens/Top';
+import Navbar from './components/Navbar';
+import Quiz from './screens/Quiz';
 
 function App() {
   return ( //
     <div className="App">
+      <Navbar />
       <Top />
+      <Quiz />
       {/* <Top></Top> */}
       //
     </div>
